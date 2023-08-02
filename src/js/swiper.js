b2324176c1d0e88d343155b17e0d7dca72ec94ce
DimaMen265 <menzak87@gmail.com>
2023-08-02 23:13:58 +0200

    added a swiper to the reviews block

---
+++ src/js/swiper.js
@@ -0,0 +1,66 @@
+function initSwiper(slidesPerView, spaceBetween, autoplayDelay, direction) {
+    return new Swiper('.swiper', {
+      slidesPerView: slidesPerView,
+      spaceBetween: spaceBetween,
+      speed: 1000,
+      loop: true,
+      mousewheel: true,
+      keyboard: true,
+      autoplay: {
+        delay: autoplayDelay,
+          disableOnInteraction: false,
+        },
+      
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        },
+      
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+      
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        },
+      
+      breakpoints: {
+        768: {
+              autoplay: {
+               reverseDirection: true,
+           }
+           },
+        },
+    });
+  }
+
+  function handleMediaQueries() {
+    const mediaQuery375 = window.matchMedia('(min-width: 375px)');
+    const mediaQuery768 = window.matchMedia('(min-width: 768px)');
+    const mediaQuery1280 = window.matchMedia('(min-width: 1280px)');
+
+    let swiper;
+
+    const breakpointChecker = () => {
+    if (mediaQuery1280.matches) {
+      // Desktop
+      if (swiper) swiper.destroy(true, true);
+      swiper = initSwiper(3, 16, 3000, 'horizontal');
+    } else if (mediaQuery768.matches) {
+      // Tablet
+      if (swiper) swiper.destroy(true, true);
+      swiper = initSwiper(2, 16, 3000, 'horizontal');
+    } else if (mediaQuery375.matches) {
+      // Mobile
+      if (swiper) swiper.destroy(true, true);
+      swiper = initSwiper(1, 8, 3000, 'horizontal');
+    }
+  };
+
+    breakpointChecker();
+
+    window.addEventListener('resize', breakpointChecker);
+  }
+
+  handleMediaQueries();
