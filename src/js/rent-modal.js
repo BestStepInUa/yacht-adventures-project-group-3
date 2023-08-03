a882eca0cfd027f036e6c2a7fedb48081fb730b4
Kyrylo Matkash <kyrylo.mm@gmail.com>
2023-08-04 01:32:29 +0200

    .

--- src/js/rent-modal.js
+++ src/js/rent-modal.js
@@ -1,112 +1,115 @@
-    let isValid = false;
-    const minLength = 2;
-    const maxLength = 30;
-    const namePattern = /^[A-Za-z]+(?:\s[A-Za-z]+)?$/;
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const phonePattern = /^\+?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+let isValid = false;
+const minLength = 2;
+const maxLength = 30;
+const namePattern = /^[A-Za-z]+(?:\s[A-Za-z]+)?$/;
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const phonePattern = /^\+?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
 
-    (() => {
-      const refs = {
-        openModalBtn: document.querySelector("[data-modal-open]"),
-        closeModalBtn: document.querySelector("[data-modal-close]"),
-        modal: document.querySelector("[data-modal]"),
-      };
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
-      refs.openModalBtn.addEventListener("click", (event) => {
-        event.preventDefault();
-        validateInput();
-        submitForm(event);
-      });
+  refs.openModalBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    validateInput();
+    submitForm(event);
+  });
 
-      refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-      function toggleModal() {
-        if (isValid) {
-          refs.modal.classList.toggle("is-hidden");
-          if (!refs.modal.classList.contains("is-hidden")) {
-            playSoundIfFieldsFilled();
-          }
-        }
+  function toggleModal() {
+    if (isValid) {
+      refs.modal.classList.toggle("is-hidden");
+      if (!refs.modal.classList.contains("is-hidden")) {
+        playSoundIfFieldsFilled();
       }
+    }
+  }
 
-      function submitForm(event) {
-        event.preventDefault();
-        if (isValid) {
-          // Your submission logic here (you can make an API call or any other actions needed)
-          // ...
+  function submitForm(event) {
+    event.preventDefault();
+    if (isValid) {
+      // Your submission logic here (you can make an API call or any other actions needed)
+      // ...
 
-          // Clear form inputs after successful submission
-          clearFormInputs();
+      // Play melody after submission
+      playMelody();
 
-          // Show the modal or do any other actions you want after successful submission
-          toggleModal();
-        }
-      }
+      // Clear form inputs after successful submission
+      clearFormInputs();
 
-      function clearFormInputs() {
-        const form = document.getElementById("rent-yacht-form");
-        form.reset();
-      }
+      // Show the modal or do any other actions you want after successful submission
+      toggleModal();
+    }
+  }
 
-      function validateInput() {
-        const inputName = document.getElementById("user-name");
-        const inputEmail = document.getElementById("user-email");
-        const inputPhone = document.getElementById("user-phone");
+  function clearFormInputs() {
+    const form = document.getElementById("rent-yacht-form");
+    form.reset();
+  }
 
-        const inputNameValue = inputName.value.trim();
-        const inputEmailValue = inputEmail.value.trim();
-        const inputPhoneValue = inputPhone.value.trim();
+  function validateInput() {
+    const inputName = document.getElementById("user-name");
+    const inputEmail = document.getElementById("user-email");
+    const inputPhone = document.getElementById("user-phone");
 
-        if (
-          inputNameValue.length < minLength ||
-          inputEmailValue.length > maxLength ||
-          inputEmailValue.length < minLength ||
-          inputPhoneValue.length > maxLength ||
-          inputPhoneValue.length < minLength ||
-          inputNameValue.length > maxLength
-        ) {
-          isValid = false;
-        } else {
-          isValid = true;
-        }
+    const inputNameValue = inputName.value.trim();
+    const inputEmailValue = inputEmail.value.trim();
+    const inputPhoneValue = inputPhone.value.trim();
 
-        if (!namePattern.test(inputNameValue)) {
-          isValid = false;
-          inputName.classList.add('error-border');
-        } else {
-          inputName.classList.remove('error-border');
-        }
+    if (
+      inputNameValue.length < minLength ||
+      inputEmailValue.length > maxLength ||
+      inputEmailValue.length < minLength ||
+      inputPhoneValue.length > maxLength ||
+      inputPhoneValue.length < minLength ||
+      inputNameValue.length > maxLength
+    ) {
+      isValid = false;
+    } else {
+      isValid = true;
+    }
 
-        if (!emailPattern.test(inputEmailValue)) {
-          isValid = false;
-          inputEmail.classList.add('error-border');
-        } else {
-          inputEmail.classList.remove('error-border');
-        }
+    if (!namePattern.test(inputNameValue)) {
+      isValid = false;
+      inputName.classList.add('error-border');
+    } else {
+      inputName.classList.remove('error-border');
+    }
 
-        if (!phonePattern.test(inputPhoneValue)) {
-          isValid = false;
-          inputPhone.classList.add('error-border');
-        } else {
-          inputPhone.classList.remove('error-border');
-        }
-      }
+    if (!emailPattern.test(inputEmailValue)) {
+      isValid = false;
+      inputEmail.classList.add('error-border');
+    } else {
+      inputEmail.classList.remove('error-border');
+    }
 
-      function playSoundIfFieldsFilled() {
-        const inputName = document.getElementById("user-name");
-        const inputEmail = document.getElementById("user-email");
-        const inputPhone = document.getElementById("user-phone");
+    if (!phonePattern.test(inputPhoneValue)) {
+      isValid = false;
+      inputPhone.classList.add('error-border');
+    } else {
+      inputPhone.classList.remove('error-border');
+    }
+  }
 
-        const inputNameValue = inputName.value.trim();
-        const inputEmailValue = inputEmail.value.trim();
-        const inputPhoneValue = inputPhone.value.trim();
+  function playMelody() {
+    // Replace this with your sound-playing logic for the melody
+    // For simplicity, we'll assume an HTML5 audio element with ID "clickSound"
+    const clickSound = document.getElementById("clickSound");
+    clickSound.currentTime = 0;
+    clickSound.play();
+  }
 
-        if (inputNameValue !== "" && inputEmailValue !== "" && inputPhoneValue !== "") {
-          // Replace this with your sound-playing logic
-          // For simplicity, we'll assume an HTML5 audio element with ID "clickSound"
-          const clickSound = document.getElementById("clickSound");
-          clickSound.currentTime = 0;
-          clickSound.play();
-        }
-      }
-    })();
+  function playSoundIfFieldsFilled() {
+    const inputName = document.getElementById("user-name");
+    const inputEmail = document.getElementById("user-email");
+    const inputPhone = document.getElementById("user-phone");
+
+    const inputNameValue = inputName.value.trim();
+    const inputEmailValue = inputEmail.value.trim();
+    const inputPhoneValue = inputPhone.value.trim();
+  }
+})();
